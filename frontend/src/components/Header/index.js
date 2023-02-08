1aeb8e96b614b6ec822dde037bc0868c79a0d357
Godnelson <luizgatohd@gmail.com>
2023-02-07 23:46:49 -0300

    inital_text_resize_implement

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,16 +1,24 @@
 import { Container, Item } from './styles'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fontSize } from '@mui/system'
 
 export default () => {
+    function AttFontSize(){
+        const windowSize = window.innerWidth
+        useFont(windowSize < 420 ? 25 : 32)
+        console.log('entrando')
+    }
+    const [fontSizeSite, useFont] = useState(32)
     const date = new Date()
     const hour = date.getHours()
     const message = hour >= 0 && hour < 12 ? 'Good Morning' : hour >= 12 && hour < 18 ? 'Good Afternoon' : 'Good Night' 
+    window.addEventListener('resize', AttFontSize)
     return(
         <Container>
-            <Item>
+            <Item style={{fontSize: fontSizeSite}}>
                 CadUsers
             </Item>
-            <Item>
+            <Item style={{fontSize: fontSizeSite}}>
                 {message}
             </Item>
         </Container>
