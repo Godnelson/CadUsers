f12c6335702abe89b5c56ceac5410617a78bc04d
Godnelson <luizgatohd@gmail.com>
2023-02-08 22:41:29 -0300

    list_item_created

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { AddButton, ButtonContainer } from "./styles";
+import {
+  AddButton,
+  ButtonContainer,
+  ListAge,
+  ListBox,
+  ListContainer,
+  ListItem,
+  ListItemLeftContainer,
+  ListName,
+  ListOptions,
+  ListOptionsIcons,
+} from "./styles";
 
-export default () =>{
-    return(
-        <ButtonContainer>
-            <AddButton >
-                +
-            </AddButton>
-        </ButtonContainer>
-    )
-}
+export default () => {
+  return (
+    <ListContainer>
+      <ButtonContainer>
+        <AddButton>+</AddButton>
+      </ButtonContainer>
+      <ListBox>
+        <ListItem>
+          <ListName>Name</ListName>
+          <ListItemLeftContainer>
+            <ListAge>Age</ListAge>
+            <ListOptions>
+                <ListOptionsIcons>
+                    <img src={"logo192.png"} width={'100%'}/>
+                </ListOptionsIcons>
+                <ListOptionsIcons>
+                    <img src={"logo192.png"} width={'100%'}/>
+                </ListOptionsIcons>
+            </ListOptions>
+          </ListItemLeftContainer>
+        </ListItem>
+      </ListBox>
+    </ListContainer>
+  );
+};
--- frontend/src/components/List/styles.js
+++ frontend/src/components/List/styles.js
@@ -1,6 +1,14 @@
 import styled from "@emotion/styled";
 import { Button } from "@mui/material";
 
+export const ListContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`
+
 export const ButtonContainer = styled.div`
     display: flex;
     align-items: center;
@@ -21,3 +29,60 @@ export const AddButton = styled(Button)({
         backgroundColor:'#FFF',
     }
 })
+
+export const ListBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin-top: 8vh;
+    box-sizing: border-box;
+    padding-left: 8vw;
+    padding-right: 8vw;
+`
+
+export const ListItem = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+`
+
+export const ListName = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    font-size: 25px;
+    width: 10%;
+`
+
+export const ListItemLeftContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 25px;
+    width: 10%;
+`
+
+export const ListAge = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    border-right: 1px solid black;
+    margin-right: 0.5vw;
+    font-size: 25px;
+    width: 49%;
+`
+
+export const ListOptions = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
+`
+
+export const ListOptionsIcons = styled.div`
+    display: flex;
+    align-items: center;
+    width: 45%;
+`
